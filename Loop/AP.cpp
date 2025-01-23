7bd33645d7091a64dce5ab842f61e0df91f7cd30
Suryabhan Singh <suryabhan283808@gmail.com>
2025-01-23 23:18:14 +0530

    Add C++ programs to find Arithmetic and Geometric Progression series

---
+++ Loop/AP.cpp
@@ -0,0 +1,16 @@
+/* find the AP series*/
+#include <iostream>
+using namespace std;
+int main(){
+    int a,d,n;
+    cout<<"Enter a :";
+    cin>>a;
+    cout<<"Enter d :";
+    cin>>d;
+    cout<<"Enter n :";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<a<<" ";
+        a+=d;
+    }
+}
---
+++ Loop/GP.cpp
@@ -0,0 +1,16 @@
+/*find GP series*/
+#include <iostream>
+using namespace std;
+int main(){
+    int a,r,n;
+    cout<<"Enter a :";
+    cin>>a;
+    cout<<"Enter r :";
+    cin>>r;
+    cout<<"Enter n :";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<a<<" ";
+        a*=r;
+    }
+}
