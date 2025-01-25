5e522cd96fd53a055284fd7afc1808e3f3c9c085
Suryabhan Singh <suryabhan283808@gmail.com>
2025-01-26 00:30:58 +0530

    find the 4th element of an array

---
+++ Arrays/4th_element_array.cpp
@@ -0,0 +1,9 @@
+// fint the 4th element of the array
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[5]={2,3,4,5,6}; // array of 5 elements size can fixed in the declaration
+    //print the 4th element of the array
+    cout<<arr[3];
+    
+}
