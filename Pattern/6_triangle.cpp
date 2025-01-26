730ff2834e84b187181939ccdb22b7c5fcb90be1
Suryabhan Singh <suryabhan283808@gmail.com>
2025-01-26 14:33:36 +0530

    program to print an equilateral triangle pattern

---
+++ Pattern/6_triangle.cpp
@@ -0,0 +1,17 @@
+// print a equilateral triange pattern
+#include <iostream>
+using namespace std;
+int main(){
+    int row;
+    cout<<"Enter the number of rows: ";
+    cin>>row;
+    for(int i=1; i<=row; i++){
+        for(int j=1; j<=row-i; j++){
+            cout<<" ";
+        }
+        for(int j=1; j<=i; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
