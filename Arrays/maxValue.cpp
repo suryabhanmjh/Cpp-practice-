6688fbb48f29da7d87a7ad875b2cb7762f864ba3
Suryabhan Singh <suryabhan283808@gmail.com>
2025-01-28 16:32:37 +0530

    find and display the maximum value in an array

---
+++ Arrays/maxValue.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[]={3,6,9,2,5,7};
+    int max=arr[0];
+    for(int i=1;i<6;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    cout<<"Max value is: "<<max;
+}
