1944b1a67c3fc8a866ce72fad7e5b7d411230976
Suryabhan Singh <suryabhan283808@gmail.com>
2025-01-28 18:59:08 +0530

    minimum value

--- Arrays/minValu.cpp
+++ Arrays/minValu.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int arr[]={4,6,8,2,5,7};
+    int arr[]={3,6,8,1,5,7};
     int min=arr[0];
     for(int i=1;i<6;i++){
         if(arr[i]<min){
