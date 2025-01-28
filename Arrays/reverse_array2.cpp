0a60f0f1b8650e1be3bb79427db4c704e6b69c8c
Suryabhan Singh <suryabhan283808@gmail.com>
2025-01-28 19:31:09 +0530

    Add program to reverse an array using swapping

---
+++ Arrays/reverse_array2.cpp
@@ -0,0 +1,18 @@
+// To reverse an array using a temporary array or swapping
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[]={4,6,9,3,7};
+    int i=0;
+    int j=4;
+    while(i<j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+        i++;
+        j--;
+     }
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+}
