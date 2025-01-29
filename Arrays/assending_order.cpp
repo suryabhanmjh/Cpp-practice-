6196fe5a553deaee1a875efffbb3939bdb7b3931
Suryabhan Singh <suryabhan283808@gmail.com>
2025-01-29 19:00:14 +0530

    array in ascending order

---
+++ Arrays/assending_order.cpp
@@ -0,0 +1,18 @@
+// Arrange the elements of an array in ascending order
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[]={4,6,9,3,7};
+    for(int i=0;i<5;i++){
+        for(int j=i+1;j<5;j++){
+            if(arr[i]>arr[j]){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+}
