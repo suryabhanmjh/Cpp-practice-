50be3bfdae6312fee5af3786f142e48478b430ec
Suryabhan Singh <suryabhan283808@gmail.com>
2025-01-30 23:01:55 +0530

    Add program to find multiple missing numbers in a given array

--- Arrays/Find_multiple_missing_num2.cpp
+++ Arrays/Find_multiple_missing_num2.cpp
@@ -1,1 +1,15 @@
 // Find the multiple missing numbers in given array of 1 to n
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[]={1,2,5,6,8,11};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<size;i++){
+       if(arr[i+1]-arr[i]!=1){
+           for(int j=arr[i]+1;j<arr[i+1];j++){
+               cout<<j<<" ";
+           }
+       }
+    
+    }
+}
