71657b652ec7c9b4f5cf11b8c8c42d7f1c3665b5
Suryabhan Singh <suryabhan283808@gmail.com>
2025-01-31 18:57:45 +0530

    duplicate finding logic to improve clarity and efficiency

--- Arrays/find_duplicate.cpp
+++ Arrays/find_duplicate.cpp
@@ -5,15 +5,19 @@ int main(){
     int arr[]={1,2,3,1,4,5,6,2,3,1,1};
     int size=sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<size;i++){
-        if(arr[i]!=-1){
-            for(int j=i+1;j<size;j++){
-                if(arr[i]==arr[j]){
-                    
-                    arr[j]=-1;
-                }
-                cout<<arr[i]<<endl;
+        bool check=true;
+        for(int j=i+1;j<size;j++){
+        if(arr[j]==arr[i]){
+            arr[j]=-1;
+            check=false;
             }
-        }
+            
+            }
+            if (check==false && arr[i]!=-1){
+                cout<<arr[i]<<" ";
+            }
     }
-}
+
+    }
+
     
