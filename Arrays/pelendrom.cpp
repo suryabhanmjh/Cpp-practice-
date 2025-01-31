21c5dd2b1c812f674ae4a29f049ad65fd8156683
Suryabhan Singh <suryabhan283808@gmail.com>
2025-01-31 19:07:05 +0530

    if an array is a palindrome

---
+++ Arrays/pelendrom.cpp
@@ -0,0 +1,17 @@
+// find pelendrom in an array
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,3,2,1,};
+   int i=0;
+   int j=5;
+   while(i<j){
+       if(arr[i]!=arr[j]){
+           cout<<"Not pelendrom";
+           return 0;
+       }
+       i++;
+       j--;
+   }
+    cout<<"Pelendrom";
+}
