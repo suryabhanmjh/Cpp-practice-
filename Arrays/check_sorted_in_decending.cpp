4a189eebff73862f0c9933ddda36526a6b8c12a4
Suryabhan Singh <suryabhan283808@gmail.com>
2025-02-03 22:23:02 +0530

    check for descending order in a new file

---
+++ Arrays/check_sorted_in_decending.cpp
@@ -0,0 +1,20 @@
+// Check if an array is sorted in descending order.
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[]={7,6,5,4,3,2,1};
+    int n=7;
+    int count=0;
+    for(int i=0;i<n-1;i++){
+        if(arr[i]<arr[i+1]){
+            count=1;
+            break;
+        }
+    }
+    if(count==1){
+        cout<<"Array is not sorted in descending order";
+    }
+    else{
+        cout<<"Array is sorted in descending order";
+    }
+}
--- Arrays/majority_element.cpp
+++ Arrays/majority_element.cpp
@@ -2,19 +2,24 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int arr[]={1,2,3,1,4,5,6,2,3,1,1};
-    int size=sizeof(arr)/sizeof(arr[0]);
-    for(int i=0;i<size;i++){
-        int count=1;
-        for(int j=i+1;j<size;j++){
-        if(arr[j]==arr[i]){
-            count++;
-            }
-            
-            }
-            if (count>size/2){
-                cout<<arr[i]<<" ";
-            }
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    int count = 1, res = 0;
+    for(int i=1;i<n;i++){
+        if(arr[res] == arr[i]) count++;
+        else count--;
+        if(count == 0){
+            res = i;
+            count = 1;
+        }
     }
-
+    count = 0;
+    for(int i=0;i<n;i++){
+        if(arr[res] == arr[i]) count++;
     }
+    if(count > n/2) cout<<arr[res]<<endl;
+    else cout<<"No majority element"<<endl;
+    return 0;
+}
