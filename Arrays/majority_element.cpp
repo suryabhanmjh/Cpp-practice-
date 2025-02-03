4a0d05fbec34897ba8d44110b05302485b74846a
Suryabhan Singh <suryabhan283808@gmail.com>
2025-02-03 19:14:57 +0530

    majority_element

--- Arrays/majority_element.cpp
+++ Arrays/majority_element.cpp
@@ -1,20 +1,20 @@
-// // Find the majority element in the given array
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int arr[]={1,2,3,1,4,5,6,2,3,1,1};
-//     int size=sizeof(arr)/sizeof(arr[0]);
-//     for(int i=0;i<size;i++){
-//         int count=1;
-//         for(int j=i+1;j<size;j++){
-//         if(arr[j]==arr[i]){
-//             count++;
-//             }
+// Find the majority element in the given array
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,1,4,5,6,2,3,1,1};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<size;i++){
+        int count=1;
+        for(int j=i+1;j<size;j++){
+        if(arr[j]==arr[i]){
+            count++;
+            }
             
-//             }
-//             if (count>size/2){
-//                 cout<<arr[i]<<" ";
-//             }
-//     }
+            }
+            if (count>size/2){
+                cout<<arr[i]<<" ";
+            }
+    }
 
-//     }
+    }
