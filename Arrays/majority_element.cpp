7bd928770bcf0b00b0ec5d56230aa952954cfd16
Suryabhan Singh <suryabhan283808@gmail.com>
2025-02-03 22:26:22 +0530

    majority_element.cpp to use a static array and improve readability

--- Arrays/majority_element.cpp
+++ Arrays/majority_element.cpp
@@ -2,24 +2,32 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++) cin>>arr[i];
-    int count = 1, res = 0;
+    int arr[]={2,2,1,1,1,2,2};
+    int n=7;
+    int count=1;
+    int res=0;
     for(int i=1;i<n;i++){
-        if(arr[res] == arr[i]) count++;
-        else count--;
-        if(count == 0){
-            res = i;
-            count = 1;
+        if(arr[res]==arr[i]){
+            count++;
+        }
+        else{
+            count--;
+        }
+        if(count==0){
+            res=i;
+            count=1;
         }
     }
-    count = 0;
+    count=0;
     for(int i=0;i<n;i++){
-        if(arr[res] == arr[i]) count++;
+        if(arr[res]==arr[i]){
+            count++;
+        }
     }
-    if(count > n/2) cout<<arr[res]<<endl;
-    else cout<<"No majority element"<<endl;
-    return 0;
+    if(count>n/2){
+        cout<<"Majority element is: "<<arr[res];
+    }
+    else{
+        cout<<"No majority element";
+    }
 }
