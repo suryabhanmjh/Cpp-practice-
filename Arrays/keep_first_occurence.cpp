ee5dda21082ea599f9b2a061ea495ef70c262711
Suryabhan Singh <suryabhan283808@gmail.com>
2025-02-03 19:12:38 +0530

    add algorithms to keep unique elements and first occurrence in an array

---
+++ Arrays/keep_first_occurence.cpp
@@ -0,0 +1,21 @@
+// Keep First Occurrence of Elements in the Array
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,1,4,5,6,2,3,1,1};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<size;i++){
+        bool check=true;
+        for(int j=i+1;j<size;j++){
+        if(arr[j]==arr[i]){
+            arr[j]=-1;
+            check=false;
+            }
+            
+            }
+            if (check==false && arr[i]!=-1){
+                cout<<arr[i]<<" ";
+            }
+    }
+
+    }
