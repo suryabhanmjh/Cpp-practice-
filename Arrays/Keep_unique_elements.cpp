846e44e3c683daf1322de45363d8aeb8e1538cb2
Suryabhan Singh <suryabhan283808@gmail.com>
2025-02-03 19:13:39 +0530

    Keep_unique_elements.cpp

--- Arrays/Keep_unique_elements.cpp
+++ Arrays/Keep_unique_elements.cpp
@@ -1,21 +1,21 @@
-// // Keep unique elements in the array
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int arr[]={1,2,3,1,4,5,6,2,3,1,1};
-//     int size=sizeof(arr)/sizeof(arr[0]);
-//     for(int i=0;i<size;i++){
-//         bool check=true;
-//         for(int j=i+1;j<size;j++){
-//         if(arr[j]==arr[i]){
-//             arr[j]=-1;
-//             check=false;
-//             }
+// Keep unique elements in the array
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,1,4,5,6,2,3,1,1};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<size;i++){
+        bool check=true;
+        for(int j=i+1;j<size;j++){
+        if(arr[j]==arr[i]){
+            arr[j]=-1;
+            check=false;
+            }
             
-//             }
-//             if (check==false && arr[i]!=-1){
-//                 cout<<arr[i]<<" ";
-//             }
-//     }
+            }
+            if (check==false && arr[i]!=-1){
+                cout<<arr[i]<<" ";
+            }
+    }
 
-//     }
+    }
