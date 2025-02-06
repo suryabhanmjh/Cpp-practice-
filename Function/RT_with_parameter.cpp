78f10ac0535aa4bd24e71327640b8bac79777621
Suryabhan Singh <suryabhan283808@gmail.com>
2025-02-06 23:30:17 +0530

    refactor: reorganize function implementations; remove old versions and add new files

---
+++ Function/RT_with_parameter.cpp
@@ -0,0 +1,13 @@
+// Return type function with peremeter
+#include <iostream>
+using namespace std;
+int sum (int a, int b){
+    int c=a+b;
+    return c;
+}
+
+int main(){
+    int num1=30, num2= 15;
+    cout<<sum(num1,num2);
+
+}
