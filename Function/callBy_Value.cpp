00fc1d45e08e588ed9a9080e37593e93367066c4
Suryabhan Singh <suryabhan283808@gmail.com>
2025-02-08 23:55:11 +0530

    add call by value example in new file

---
+++ Function/callBy_Value.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+// call by value
+int plusone(int num){      //formal argument
+   num+=num+1;
+   return num;
+}
+
+int main(){
+    int a=10,num; 
+    cout<<plusone(a)<<endl;    //actual argument
+    cout<<a;
+}
