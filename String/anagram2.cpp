3e09fc90ee480048bd26eb3c1ae8568d8b5e985d
Suryabhan Singh <suryabhan283808@gmail.com>
2025-02-12 19:13:04 +0530

    second method for anagram checking using sorting

--- String/anagram2.cpp
+++ String/anagram2.cpp
@@ -1,1 +1,21 @@
-// anag
+// anagram 2nd methods by sorting
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    string s1 = "listen";
+    string s2 = "silent";
+
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+
+    if (s1 == s2) {
+        cout << "Anagram";
+    }
+     else {
+        cout << "Not anagram";
+    }
+   
+}
