f79cd6cc1a1590bb871cee8845ea5b0aa124fb0d
Suryabhan Singh <suryabhan283808@gmail.com>
2025-02-13 16:20:27 +0530

    refactor balanced parentheses validation program and remove unused executable

--- String/balence_paranthesis.cpp
+++ String/balence_paranthesis.cpp
@@ -5,5 +5,45 @@ using namespace std;
 
 int main() {
     string s="{[()]}";
-    int cur = 0, sq = 0, round = 0;
-  
+    int cur = 0, sq = 0, par = 0;
+    int size=s.length();
+
+    for(int i=0;i<size;i++){
+        if(s[i] == '{'){
+            cur++;
+        }
+        else if (s[i] == '('){
+            par++;
+        }
+        else if (s[i] == '['){
+            sq++;
+        }
+        else if (s[i] == '}'){
+            cur--;
+            if(cur<0){
+                cout<<"NO"<<endl;
+                return 0;
+            }
+        }
+        else if (s[i] == ')'){
+            par--;
+            if(par<0){
+                cout<<"NO"<<endl;
+                return 0;
+            }
+        }
+        else if (s[i] == ']'){
+            sq--;
+            if(sq<0){
+                cout<<'NO'<<endl;
+                return 0;
+            }
+        }
+    }
+    if(cur==0 && par==0 && sq==0){
+        cout<<"Balance"<<endl;
+    }
+    else{
+        cout<<"Not Balance"<<endl;
+    }
+}
