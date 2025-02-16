c04d69cfd6fb492ac874272a73dd6bb5c0762cb3
Suryabhan Singh <suryabhan283808@gmail.com>
2025-02-16 21:06:44 +0530

    add program to display ASCII values and their corresponding characters

---
+++ String/sky_value.cpp
@@ -0,0 +1,14 @@
+// sky value
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main(){
+    int a=65;
+    cout<<char(a);
+
+    for(int i=0; i<=127;i++){
+        cout<<" = "<<char(i)<<endl;
+    }
+}
